5e1503e900ed2871956d6dce4eecf3529e493d94
Subha Halder <133703969+SubhaHalder-spec@users.noreply.github.com>
2024-08-10 16:57:16 +0530

    Update UniLRT.R

--- R/UniLRT.R
+++ R/UniLRT.R
@@ -6,11 +6,74 @@
 #' @return Test statistic value numeric
 #' @return Result Character
 #' @details Testing of H_0:mu_1 = mu_2 = ... = mu_k vs H_1:mu_1 <=.....<= mu_(h-1)<= mu_h >= mu_(h+1)>=....>= mu_k (at least one strict inequality), where mu_i represents the population means of the i-th treatment. The input consists of two variables: sample_data and significance_level. The output consists of the critical value, the UniLRT test statistic value, and the result, which indicates whether to reject or not reject the null hypothesis.
-#' @import stats
-#' @importFrom Iso ufit
+#' @importFrom Iso pava
+#' @importFrom stats qchisq
 #' @author Subha Halder
 UniLRT <- function(sample_data, significance_level){
   set.seed(456)
+  R_MLE <- function(X, n) {
+    X1 <- X[-1]
+    n1 <- n[-1]
+    sorted_indices <- order(X1)
+    X1_sorted <- X1[sorted_indices]
+    n1_sorted <- n1[sorted_indices]
+    A <- numeric(length(X1_sorted))
+    for (j in 2:length(X)) {
+      A[j-1] <- (n[1] * X[1] + sum(n1_sorted[1:(j - 1)] * X1_sorted[1:(j - 1)])) /
+        (n[1] + sum(n1_sorted[1:(j - 1)]))
+    }
+    if (all(X1 >= X[1])) {
+      new_X <- X
+    } else if (A[length(X)-2] >= X1_sorted[length(X)-1]) {
+      X <- rep(A[length(X)-1], length(X))
+      new_X <- X
+    } else {
+      comparisons <- logical(length(X1_sorted) - 1)
+      comparisons1 <- logical(length(X1_sorted) - 1)
+      stored_values <- numeric(0)
+      for (k in 1:(length(X1_sorted) - 1)) {
+        comparisons[k] <- A[k] < X1_sorted[k + 1]
+        if(comparisons1[k] <- A[k] < X1_sorted[k + 1]) {
+          for (s in 1:k) {
+            stored_values[s] <- X1_sorted[s]
+          }
+          break
+        }
+      }
+      selected_A_values <- A[comparisons]
+      X[1] <- selected_A_values[1]
+      for (l in 2:length(X)) {
+        if (X[l] %in% stored_values) {
+          X[l] <- selected_A_values[1]
+        }
+      }
+      new_X <- X
+    }
+    return(new_X)
+  }
+
+  unimodal <- function(X, w, lmode) {
+    if(lmode==length(X)){
+      new_X <- Iso::pava(X,w)
+    } else if (lmode==1){
+      new_X <- -(Iso::pava(-X,w))
+    } else{
+      X1 <- X[1:(lmode-1)]
+      X2 <- X[(lmode+1):length(X)]
+      w1 <- w[1:(lmode-1)]
+      w2 <- w[(lmode+1):length(X)]
+      newX1 <- Iso::pava(X1,w1)
+      newX2 <- -(Iso::pava(-X2,w2))
+      Y <- c(X[lmode],newX1,newX2)
+      w_new<- c(w[lmode],w1,w2)
+      v <- -(R_MLE(-Y,w_new))
+      max_v <- v[-1]
+      new_X1 <- max_v[1:length(X1)]
+      new_X2 <- max_v[(length(X1)+1):length(max_v)]
+      new_X <- c(new_X1,v[1],new_X2)
+    }
+    return(new_X)
+  }
   LRT_H0_new <- function(sample_data_list) {
     means <- sapply(sample_data_list, mean)
     sample_sizes <- sapply(sample_data_list, length)
@@ -42,7 +105,7 @@ UniLRT <- function(sample_data, significance_level){
     var0 <- sapply(1:length(sample_data_list), function(i) (sum((sample_data_list[[i]] - mu0[i])^2)) / n[i])
     w0 <- n / var0
     repeat {
-      new_mu0 <- ufit(y = mu0, imode = ceiling((length(sample_data_list))/2), w = w0)[[2]]
+      new_mu0 <- unimodal(X = sapply(sample_data_list, mean), w = w0, lmode = ceiling(length(sample_data_list)/2))
       new_var0 <- sapply(1:length(sample_data_list), function(i) (sum((sample_data_list[[i]] - new_mu0[i])^2)) / n[i])
       new_w0 <- n / new_var0
 
@@ -57,7 +120,6 @@ UniLRT <- function(sample_data, significance_level){
 
     return(var0)
   }
-
   sample_data <- lapply(sample_data, function(x) x[!is.na(x)])
   num_samples = 20000
   num_datasets <- length(sample_data)
